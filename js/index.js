be5f82df21ec01bacddfaf9e8a949243df36ec70
Muathasim Mohamed <muth4muathasim@gmail.com>
2019-06-24 22:29:22 +0530

    gallery page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,36 @@
 function onNavClick(option) {
     document.getElementsByClassName("selected")[0].classList.remove("selected");
     document.getElementById(option).classList.add("selected");
+
+    scrollToElement("#" + option + "-section");
 }
+
+var scrollToElement = function(el, ms){
+    var speed = (ms) ? ms : 600;
+    console.log($(el).offset().top);
+    $('html,body').animate({
+        scrollTop: $(el).offset().top
+    }, speed);
+}
+
+function onScroll() {
+    let position = window.pageYOffset || document.documentElement.scrollTop;
+    let aboutPosition = 300;
+    let galleryPosition = $("#gallery").offset().top + 300;
+
+    if (position <= aboutPosition) {
+        document.getElementsByClassName("selected")[0].classList.remove("selected");
+        document.getElementById("about").classList.add("selected");
+    } else if (position <= galleryPosition) {
+        document.getElementsByClassName("selected")[0].classList.remove("selected");
+        document.getElementById("gallery").classList.add("selected");
+    }
+
+    if (position === 0) {
+        document.getElementsByClassName("navbar")[0].classList.add("transparent");
+    } else {
+        document.getElementsByClassName("navbar")[0].classList.remove("transparent");
+    }
+}
+
+document.addEventListener("scroll", onScroll);
